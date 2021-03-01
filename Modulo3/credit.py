e0e24d07861ef59598f91ec991e7ef8b0f0f935e
luisavalos0103 <55954683+luisavalos0103@users.noreply.github.com>
2021-02-28 19:00:31 -0500

    Add files via upload

---
+++ Modulo3/credit.py
@@ -0,0 +1,44 @@
+# Algoritmo de Luhn en python
+
+def luhn():
+    
+    n= input("Ingrese el número de su tarjeta: ")
+
+    suma = 0
+
+    alt = 0
+
+    i = len(n) - 1
+
+    valor = 0
+    
+    while i >= 0:
+
+       valor = int( n[ i ] )
+
+       if alt:
+
+          valor = valor * 2
+
+          if valor > 9:
+
+             valor = ( valor % 10 ) + 1  
+
+       suma = suma + valor
+ 
+       alt = not alt 
+
+       i -= 1
+
+# Imprime que tipo de tarjeta es 
+        
+    if suma%10 == 0 and n[0] == "4" :
+        print("VISA")
+    elif suma%10 == 0 and n[0] == "3" :
+        print("AMEX")
+    elif suma%10 == 0 and n[0] == "5" :
+        print("MASTERCARD")
+    else :
+        print("Inválido")
+        
+luhn()
